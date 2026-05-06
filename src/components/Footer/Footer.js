f262d5f3cb861e6fe4f2386c5645400e69a29d85
vange <houseofvange@gmail.com>
2026-05-06 16:20:12 -0700

    HOV: creates footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import SocialMedias from "components/Footer/SocialMedias";
+import 'components/Footer/Footer.css';
+
+
+const Footer = (props) => {
+
+    return (
+        <footer>
+            <SocialMedias />
+        </footer>
+    );
+};
+
+export default Footer;
